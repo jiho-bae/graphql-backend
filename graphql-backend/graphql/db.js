8a55d45cfd18425d5e38f2bd1b5dbb2b9bc16bbc
JihoBae <jiho@dgu.ac.kr>
2021-06-08 11:42:32 +0900

    make a suggestions func in graphql server

--- graphql-backend/graphql/db.js
+++ graphql-backend/graphql/db.js
@@ -3,6 +3,7 @@ import fetch from "node-fetch";
 const BASE_URL = "https://yts.mx/api/v2/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json?`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json?`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json?`;
 
 export const getMovies = (limit, rating) => {
   let REQUEST_URL = LIST_MOVIES_URL;
@@ -23,4 +24,12 @@ export const getMovie = (id) => {
   return fetch(REQUEST_URL)
     .then((res) => res.json())
     .then((json) => json.data.movie);
+};
+
+export const getSuggestions = (id) => {
+  let REQUEST_URL = MOVIE_SUGGESTIONS_URL;
+  REQUEST_URL += `movie_id=${id}`;
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
